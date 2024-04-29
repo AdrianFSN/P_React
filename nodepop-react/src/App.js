1222451a822aa41d3895b6e7bc6e659904dd7270
Adrián S <adriansotes@gmail.com>
2024-04-29 13:03:32 +0200

    Login works (no token storage)

--- nodepop-react/src/App.js
+++ nodepop-react/src/App.js
@@ -1,9 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import AdvertsPage from "./pages/adverts/AdvertsPage";
+import { useState } from "react";
+import { LoginPage } from "./components/shared/LoginPage";
 
 function App() {
-  return <AdvertsPage />;
+  const [isLogged, setIsLogged] = useState(false);
+
+  const handleLogin = () => setIsLogged(true);
+  return (
+    <section>
+      {isLogged ? <AdvertsPage /> : <LoginPage onLogin={handleLogin} />}
+    </section>
+  );
 }
 
 export default App;
