f024a0c77000dfca54460627f45781dd68f8309e
Adrián S <adriansotes@gmail.com>
2024-05-02 14:35:02 +0200

    feature: using backup images in Advert detail view

--- nodepop-react/src/pages/adverts/components/Advert.js
+++ nodepop-react/src/pages/adverts/components/Advert.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Advert.css";
+import ImageNotAvailable from "../../../assets/ImageNotAvailable.jpg";
 
 const Advert = ({ id, name, price, sale, tags, photo }) => {
+  const backupPhoto = ImageNotAvailable;
+  const [pictureToShow, setPictureToShow] = useState(backupPhoto);
+
+  useEffect(() => {
+    if (photo) {
+      setPictureToShow(photo);
+    }
+  }, [photo, backupPhoto]);
+
   return (
     <section className="single-advert">
       <ul key={id}>
-        {photo && <img src={`${photo}`} alt="Advert offer" />}
+        {<img src={pictureToShow} alt="Advert offer" />}
         <li>
           <h2>{name}</h2>
         </li>
